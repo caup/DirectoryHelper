82c42d95d77d476eba2a4f4938c25ac3b04b0b2b
caup <evansdljr@gmail.com>
2021-06-17 10:49:42 -0400

    first

---
+++ DirectoryHelper/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+using std::string;
+namespace fs = std::filesystem;
+
+int main()
+{
+	string dirName;
+	int subDirs;
+
+	auto path = fs::current_path();
+
+	std::cout << "Current path:\n" << path;
+
+	std::cout << "\n\nDirectory name: ";
+	std::cin >> dirName;
+	std::cout << "# of sub-directories: ";
+	std::cin >> subDirs;
+
+	if (!fs::create_directory(dirName))
+	{
+		std::cout << "\n\nFailed to create main directory.\n";
+	}
+
+	fs::current_path(dirName);
+
+	bool test = false;
+	for (int i = 1; i <= subDirs; i++)
+	{
+		if (i < 10)
+		{
+			if (!fs::create_directory("0" + std::to_string(i)))
+			{
+				test = true;
+			}	
+		}
+		else
+		{
+			if (!fs::create_directory(std::to_string(i)))
+			{
+				test = true;
+			}	
+		}
+	}
+
+	if (test)
+	{
+		std::cout << "Failed to create sub-directories.\n";
+	}
+	else
+	{
+		std::cout << "\nProcess completed successfully.\n";
+	}
+
+	std::cout << "Press enter to exit...";
+	std::cin.ignore();
+	std::cin.get();
+	return 0;
+}
